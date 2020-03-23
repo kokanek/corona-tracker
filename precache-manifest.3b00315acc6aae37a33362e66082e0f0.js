ed9f3574a2f2dc46058a9e994d89458fcc1230b0
kokaneka <kapeel.kokane@capillarytech.com>
2020-03-23 22:37:43 +0530

    Updates

---
+++ precache-manifest.3b00315acc6aae37a33362e66082e0f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c5afa2ef660604d7300029a3a0b132",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "1690cffb9b25a744fe6a",
+    "url": "/corona-tracker/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "c3fd7fe7e7d193ebc406",
+    "url": "/corona-tracker/static/css/main.322eb7e3.chunk.css"
+  },
+  {
+    "revision": "1690cffb9b25a744fe6a",
+    "url": "/corona-tracker/static/js/2.f387905e.chunk.js"
+  },
+  {
+    "revision": "c42205e1593581882fe0b77ce24b0664",
+    "url": "/corona-tracker/static/js/2.f387905e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3fd7fe7e7d193ebc406",
+    "url": "/corona-tracker/static/js/main.f5f6f4dc.chunk.js"
+  },
+  {
+    "revision": "6063feb5aea0a0534200",
+    "url": "/corona-tracker/static/js/runtime-main.22ab29d0.js"
+  }
+]);
